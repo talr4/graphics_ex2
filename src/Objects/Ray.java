85a0a672f1b65df3cbbb564447abe4d6bac73fb3
Liron Moshkovich <lironmosh@gmail.com>
2017-05-08 14:55:44 +0300

    Added Ray logic

---
+++ src/Objects/Ray.java
@@ -0,0 +1,20 @@
+package Objects;
+
+public class Ray {
+	private Point point;
+	private Vector vector;
+	
+	public Point getPoint() {
+		return point;
+	}
+	public void setPoint(Point point) {
+		this.point = point;
+	}
+	public Vector getVector() {
+		return vector;
+	}
+	public void setVector(Vector vector) {
+		this.vector = vector;
+	}
+	
+}
--- src/Objects/Vector.java
+++ src/Objects/Vector.java
@@ -1,5 +1,71 @@
 package Objects;
 
 public class Vector {
+	private double x;
+	private double y;
+	private double z;
+	
+	public Vector() {}
+	
+	public Vector(double x, double y, double z) {
+		this.x = x;
+		this.y = y;
+		this.z = z;
+		normalize();
+	}
+	public double getX() {
+		return x;
+	}
+	public void setX(double x) {
+		this.x = x;
+	}
+	public double getY() {
+		return y;
+	}
+	public void setY(double y) {
+		this.y = y;
+	}
+	public double getZ() {
+		return z;
+	}
+	public void setZ(double z) {
+		this.z = z;
+	}
+	
+	public double length(){
+		return Math.sqrt(Math.pow(getX(), 2) + Math.pow(getY(), 2)+ Math.pow(getZ(), 2));
+	}
+	
+	public void normalize(){
+		double length = length();
+		setX(getX() / length);
+		setX(getY() / length);
+		setX(getZ() / length);
+	}
+	
+	public double dotProduct(Vector other){
+		return getX()*other.getX() + getY()*other.getY() + getZ()*other.getZ();
+	}
+	
+	public Vector crossProduct(Vector other){
+		double newX = getY()*other.getZ() - getZ()*other.getY();
+		double newY = getZ()*other.getX() - getX()*other.getZ();
+		double newZ = getX()*other.getY() - getY()*other.getX();
+		return new Vector(newX,newY,newZ);
+	}
+	
+	public Vector add(Vector other){
+		double newX = getX()+other.getX();
+		double newY = getY()+other.getY();
+		double newZ = getZ()+other.getZ();
+		return new Vector(newX,newY,newZ);
+	}
+	
+	public Vector multiply(double scalar){
+		double newX = getX()*scalar;
+		double newY = getY()*scalar;
+		double newZ = getZ()*scalar;
+		return new Vector(newX,newY,newZ);
+	}
 	
 }
