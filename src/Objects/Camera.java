fb6b8e672d981cabfcc8c98b1a8d5b48dbffa949
talr4 <tal.rosler@gmail.com>
2017-05-08 21:09:37 +0300

    Revert "fix colisions"
    This reverts commit 900f7201fb9ffad0cdcae0b626afa232971b1b46.

--- src/Objects/Camera.java
+++ src/Objects/Camera.java
@@ -3,22 +3,16 @@ package Objects;
 import java.awt.*;
 
 public class Camera {
-	private Point Position;
-	private Point lookAt;
 	private Vector up;
 	private Screen screen;
 
 	
-	public Point getPosition() {
 		return Position;
 	}
-	public void setPosition(Point position) {
 		Position = position;
 	}
-	public Point getLookAt() {
 		return lookAt;
 	}
-	public void setLookAt(Point lookAt) {
 		this.lookAt = lookAt;
 	}
 	public Vector getUp() {
--- src/Objects/Material.java
+++ src/Objects/Material.java
@@ -6,29 +6,9 @@ public class Material {
 	
 	private Color diffuseColor;
 	private Color specularColor;
-<<<<<<< HEAD
 	private Color ReflectionColor;
 	private float phong;
 	private float transparency;
-=======
-	private Color reflectionColor;
-	private double phong;
-	private double transparency;
-	
-	
-	
-	public Material(Color diffuseColor, Color specularColor, Color reflectionColor, double phong, double transparency) {
-		
-		this.diffuseColor = diffuseColor;
-		this.specularColor = specularColor;
-		this.reflectionColor = reflectionColor;
-		this.phong = phong;
-		this.transparency = transparency;
-		
-	}
-	
-	
->>>>>>> parent of 6732a86... fix colisions
 	
 	// Getters and Setters
 	public Color getDiffuseColor() {
@@ -44,7 +24,6 @@ public class Material {
 		this.specularColor = specularColor;
 	}
 	public Color getReflectionColor() {
-<<<<<<< HEAD
 		return ReflectionColor;
 	}
 	public void setReflectionColor(Color reflectionColor) {
@@ -60,23 +39,6 @@ public class Material {
 		return transparency;
 	}
 	public void setTransparency(float transparency) {
-=======
-		return reflectionColor;
-	}
-	public void setReflectionColor(Color reflectionColor) {
-		this.reflectionColor = reflectionColor;
-	}
-	public double getPhong() {
-		return phong;
-	}
-	public void setPhong(double phong) {
-		this.phong = phong;
-	}
-	public double getTransparency() {
-		return transparency;
-	}
-	public void setTransparency(double transparency) {
->>>>>>> parent of 6732a86... fix colisions
 		this.transparency = transparency;
 	}
 	
