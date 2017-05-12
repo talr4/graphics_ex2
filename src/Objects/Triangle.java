c04dd6bf5a1c7a7f3a8b9e9430dd6b8a0da937b4
talr4 <tal.rosler@gmail.com>
2017-05-12 13:43:20 +0300

    add intersection with triangle

--- src/Objects/Triangle.java
+++ src/Objects/Triangle.java
@@ -36,15 +36,33 @@ public class Triangle extends Surface {
 
 	@Override
 	public Point findClosestIntesectionWithRay(Ray ray) {
+		
+		// Find plane containing the triangle
+		Vector vector1 = new Vector(vertex3.getX() - vertex1.getX(), vertex3.getY() - vertex1.getY(), vertex3.getZ() - vertex1.getZ());
+		Vector vector2 = new Vector(vertex3.getX() - vertex2.getX(), vertex3.getY() - vertex2.getY(), vertex3.getZ() - vertex2.getZ());
+		Vector normal = vector1.crossProduct(vector2);
+		double offset = normal.dotProductWithPoint(vertex1);
+		
+		Plane plane = new Plane(normal, offset, material);
+		
+		// Find intersction with plane
+		Point intersection = plane.findClosestIntesectionWithRay(ray);
+		if(intersection == null)
+		{
+			return null;
+		}
+		
+		// Check if the intersection point with the plane is inside the triangle
 		Vector v1 = new Vector(this.vertex1.getX() - ray.getPoint().getX(), this.vertex1.getY() - ray.getPoint().getY(), this.vertex1.getZ() - ray.getPoint().getZ());
 		Vector v2 = new Vector(this.vertex2.getX() - ray.getPoint().getX(), this.vertex2.getY() - ray.getPoint().getY(), this.vertex2.getZ() - ray.getPoint().getZ());
 		Vector N1 = v1.crossProduct(v2);
+		Point check = new Point(intersection.getX() - ray.getPoint().getX(), intersection.getY() - ray.getPoint().getY(), intersection.getZ() - ray.getPoint().getZ());
+		if(N1.dotProductWithPoint(check) < 0)
+		{
+			return null;
+		}
 		
-		
-		
-		
-		
-		return null;
+		return intersection;
 	}
 	
 	
